849be43af09c0def475b8cd6ef1b16629b6af75f
Bob Summerwill <bob@summerwill.net>
2025-08-10 20:59:05 -0700

    Changed temp dir for keyring download

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@
 # Linux distributions such as Ubuntu, Mint etc.
 
 # 1. Install our official public software signing key:
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg;
-cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > ~/Downloads/signal-desktop-keyring.gpg;
+cat ~/Downloads/signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 
 # 2. Add our repository to your list of repositories:
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
